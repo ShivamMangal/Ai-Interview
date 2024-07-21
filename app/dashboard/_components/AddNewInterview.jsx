88742145ecd5ccfb6df86c8d16be1f4b942ccb7a
ShivamMangal <shivammangal0708@gmail.com>
2024-07-21 14:32:54 +0530

    fix the error

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -38,7 +38,6 @@ function AddNewInterview() {
 
         const result=await chatSession.sendMessage(InputPrompt);
         const MockJsonResp=(result.response.text()).replace('```json','').replace('```','')
-        console.log(JSON.parse(MockJsonResp));
         setJsonResponse(MockJsonResp);
 
         if(MockJsonResp)
