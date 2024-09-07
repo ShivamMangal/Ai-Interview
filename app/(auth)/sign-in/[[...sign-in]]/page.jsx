aa6a69acc6da1d6179046c70bd72e9f47337db2c
ShivamMangal <shivammangal0708@gmail.com>
2024-09-07 14:26:10 +0530

    change in login pgae

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -33,8 +33,7 @@ export default function Page() {
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+        "Unlock your potential with personalized AI-driven interview practice. Sign in to get started!".
         </p>
       </div>
     </section>
